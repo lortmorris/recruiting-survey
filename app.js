536a66a86200e8592a1d6a96cb5fa68b06cea14d
cesar casas <cesarcasas@bsdsolutions.com.ar>
2019-02-01 17:46:40 -0300

    working

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const http = require('http');
 const express = require('express');
 const fs = require('fs');
+const mongojs = require('mongojs');
+const bodyParser = require('body-parser');
+
+
+const db = mongojs('mongodb://127.0.0.1/recruiting');
+const port = process.env.PORT || 5433;
 
 const app = express();
-
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json());
 app.use(express.static('./public'));
 
 const files = [
@@ -12,6 +19,7 @@ const files = [
   { name: 'Program Languages', file: 'program-languages.csv' },
   { name: 'QA Tools', file:'qa-tools.csv' },
 ];
+
 app.get('/data', (req, res) => {
   const response = files.map(f => ({
     name: f.name, options: fs.readFileSync(`${process.cwd()}/public/${f.file}`).toString().split('\r').map(i => i.trim())
@@ -19,7 +27,20 @@ app.get('/data', (req, res) => {
   res.json(response);
 });
 
+app.post('/save', (req, res) => {
+  console.info('request: ', req.body);
+  try {
+    db.answers.insert({...req.body, added: new Date() }, (err, doc) => {
+      if (err) return res.status(500).end('grrrr');
+      return res.json({ ok: true });
+    });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).end('grrr wrong format');
+  }
+
+});
 
 const server = http.createServer(app);
 
-server.listen(5432, () => console.info('running on *:5000'));
+server.listen(port, () => console.info(`running on *:${port}`));
--- public/app.js
+++ public/app.js
@@ -73,24 +73,43 @@ $(document).ready(() => {
     $('#form').submit(function(event, b){
       event.preventDefault();
 
-
+      const email = $('#userEmail').val();
       const processItems = (key) => {
           const items = $(`.${key}Item`);
           const finalData = [];
+
           for (let x=0; x < items.length; x++) {
             const sections = $(items[x]).find('.itemSection');
             const years = sections[1].value;
             finalData.push({
               title: sections[0].innerText,
               years: sections[1].value,
-              lastYear: sections[2].value
+              lastYear: sections[2].value,
+              email,
+              key,
             })
           }
           return finalData;
       };
 
-      const finalResult = ['plangs', 'jlibs', 'javafw', 'qtools'].map(i => processItems(i));
+      const finalResult = []
+      .concat(processItems('plangs'))
+      .concat(processItems('jlibs'))
+      .concat(processItems('javafw'))
+      .concat(processItems('javafw'))
+
       console.info(finalResult);
+      $.ajax({
+        type: "POST",
+        url: '/save',
+        data: JSON.stringify(finalResult),
+        contentType: "application/json; charset=utf-8",
+        success: function() {
+          console.info('subido');
+        },
+        processData: false,
+        dataType: 'json',
+      });
 
     });
 
